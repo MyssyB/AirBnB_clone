f7576746eb79aa3594a2e0bf70a6ab0da7aa3ceb
myssyb <beulahagiande@gmail.com>
2024-03-10 11:08:10 -0700

    corrections

--- console.py
+++ console.py
@@ -2,24 +2,17 @@
 import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
-    
-    def do_help(self, arg):
-        '''Displays help with commands'''
+
+    def do_EOF(self, line):
+        '''EOF defines the end of file'''
         return True
     
+    def emptyline(self):
+        return cmd.Cmd.emptyline(self)
+
     def do_quit(self, quit):
         '''Quit command to exit the program'''
         return True
-    
-    def do_emptyline(self, arg):
-        '''Method called on an input line when command is not recognized'''
-        self.do_emptyline = 'ENTER'
-
-
-
-
-    EOF = 'EOF'
-
 
 if __name__ == '__main__':
-     HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
