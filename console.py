dc8f7c8dfdb82470c74eac8e12ce86918bf9c072
myssyb <beulahagiande@gmail.com>
2024-03-10 07:08:49 -0700

    still console

--- console.py
+++ console.py
@@ -3,18 +3,23 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
     
-    def do_quit(self, quit):
-        '''to exit intepreter'''
+    def do_help(self, arg):
+        '''Displays help with commands'''
         return True
+    
+    def do_quit(self, quit):
+        '''Quit command to exit the program'''
+        return True
+    
+    def do_emptyline(self, arg):
+        '''Method called on an input line when command is not recognized'''
+        self.do_emptyline = 'ENTER'
 
-    do_exit = do_quit
 
 
 
-
-
-
+    EOF = 'EOF'
 
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+     HBNBCommand().cmdloop()
