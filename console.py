a4cc443381011f9f534bf4066e7fd1cc4b0fe24c
myssyb <beulahagiande@gmail.com>
2024-03-10 11:32:13 -0700

    end of file

--- console.py
+++ console.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 import cmd
+
+
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
 
     def do_EOF(self, line):
         '''EOF defines the end of file'''
         return True
-    
+
     def emptyline(self):
         return cmd.Cmd.emptyline(self)
 
     def do_quit(self, quit):
         '''Quit command to exit the program'''
         return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
